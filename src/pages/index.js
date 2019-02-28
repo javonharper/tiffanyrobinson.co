d107ac498fe65c0456614c2f0ec98ce92603af14
Javon Harper <javon.d.harper@gmail.com>
2019-02-27 21:17:34 -0500

    Add simple homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,22 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <SEO
+      title="Tiffany Robinson"
+      keywords={[`project`, `product`, `management`]}
+    />
+    <div
+      style={{
+        paddingTop: "3rem",
+      }}
+    >
+      <h1>My name is Tiffany Robinson.</h1>
+      <h2>Welcome to my page! More coming soon.</h2>
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
 
